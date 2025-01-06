507f9c300f032f309e22caab554def8ff41c1260
Hussein Ibrahim <167726476+Hussein-Ibrahim043@users.noreply.github.com>
2025-01-06 15:30:01 +0200

    Tool_banner.py

--- Tool_banner.py
+++ Tool_banner.py
@@ -28,7 +28,7 @@ def banner2():
     print(Fore.CYAN +   "[═══]                  Created by: " + Fore.YELLOW + "HUSSEIN RABIO:)" + Fore.CYAN + "                             [═══]")
     print(Fore.CYAN  + "                              Version: " + Fore.MAGENTA + "1.0.0" + Fore.CYAN + "             ")
     print(Fore.CYAN +    "                          Codename: " + Fore.GREEN + "'0xHussein'" + Fore.CYAN + "                \n")
-    print(Fore.CYAN +   "[═══]    Follow me on LinkedIn: " + Fore.LIGHTYELLOW_EX + "https://linkedin.com/in/hussein-ibrahim043" + Fore.CYAN + "     [═══]")
+    print(Fore.CYAN +   "[═══]    Follow me on LinkedIn: " + Fore.LIGHTYELLOW_EX + "\033[1;33mhttps://linkedin.com/in/hussein-ibrahim043\033[0m" + Fore.CYAN + "     [═══]")
     print(Fore.CYAN +   "[═══]        Follow me on Github: " + Fore.YELLOW + "\033[1;33mhttps://github.com/Hussein-Ibrahim043\033[0m" + Fore.CYAN + "        [═══]")
     print(Fore.CYAN + "██" * 42 + Fore.RESET)
 
